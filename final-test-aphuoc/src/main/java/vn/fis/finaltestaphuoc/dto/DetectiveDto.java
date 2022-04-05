cc0c46d6010b61691c9fc906a8cce265da2f758d
longn <longnguyen3131999@gmail.com>
2022-04-05 11:17:14 +0700

    final test anh phuoc

--- final-test-aphuoc/src/main/java/vn/fis/finaltestaphuoc/dto/DetectiveDto.java
+++ final-test-aphuoc/src/main/java/vn/fis/finaltestaphuoc/dto/DetectiveDto.java
@@ -1,7 +1,18 @@
 package vn.fis.finaltestaphuoc.dto;
 
 import lombok.Data;
+import vn.fis.finaltestaphuoc.entity.EmploymentStatus;
+import vn.fis.finaltestaphuoc.entity.Rank;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Data
 public class DetectiveDto {
+    private String badgeNumber;
+    private Boolean armed;
+    @Enumerated(EnumType.STRING)
+    private Rank ranking;
+    @Enumerated(EnumType.STRING)
+    private EmploymentStatus status;
 }
